f96f276eede101456121007eb53da86e82a98d58
andrea-liliana <andrea.mx1998@gmail.com>
2022-04-29 22:47:46 +0200

    Archivo principal del proyecto

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+
+const carlo = new MissionCommander("Carlo")
+const andrea = new MissionCommander("Andrea")
+const liliana = new MissionCommander("Liliana")
+
+console.log(carlo.name)
+console.log(andrea.name)
+console.log(liliana.name)
