82edf196bf0aa104898e9260be75ad972ae5055e
andrea-liliana <andrea.mx1998@gmail.com>
2022-05-01 08:23:42 +0200

    Agregar prueba de unidad a la clase

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,3 +1,5 @@
+const MissionCommander = require('./../app/missionCommander');
+
 describe("Esto es una suite de pruebas", () => {
     test('1) Create a mission commander objet', () => {
         const myMissionCommander = new MissionCommander("Woopa")
